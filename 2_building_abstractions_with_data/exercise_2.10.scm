08f8d4f98c29b77aa2e46ade8b314b67c54e5125
Jendrik Stelzner <jendrikstelzner@web.de>
2023-04-21 20:46:00 +0200

    Add a solution to Exercise 2.10.

---
+++ 2_building_abstractions_with_data/exercise_2.10.scm
@@ -0,0 +1,11 @@
+(define (div-interval x y)
+  (if (contains-zero? y)
+      (error "Division by zero in div-interval.")
+      (mul-interval
+        x
+        (make-interval (/ 1.0 (upper-bound y))
+                       (/ 1.0 (lower-bound y))))))
+
+(define (contains-zero? x)
+  (and (<= (lower-bound x) 0)
+       (>= (upper-bound x) 0)))
