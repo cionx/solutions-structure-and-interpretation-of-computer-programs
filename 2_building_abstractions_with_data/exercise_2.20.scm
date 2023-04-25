fecb4c7ec8656d7cf91c88c6335aee4c583bf5c7
Jendrik Stelzner <jendrikstelzner@web.de>
2023-04-25 02:12:12 +0200

    Add a solution to Exercise 2.20.

---
+++ 2_building_abstractions_with_data/exercise_2.20.scm
@@ -0,0 +1,12 @@
+(define (same-parity x . items)
+  (define (iter correct-parity? items)
+    (if (null? items)
+        (list) ; empty list
+        (let ((head (car items))
+              (tail (cdr items)))
+          (let ((rest (iter correct-parity? tail)))
+            (if (correct-parity? head)
+                (cons head rest)
+                rest)))))
+  (cons x (iter (if (even? x) even? odd?)
+                items)))
