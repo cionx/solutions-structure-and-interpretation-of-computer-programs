ec9fd1693f2318a7ae0abf6b63bac3b77bbd45b8
Jendrik Stelzner <jendrikstelzner@web.de>
2023-05-01 18:43:01 +0200

    Add a solution to Exercise 2.52.

---
+++ 2_building_abstractions_with_data/2.2_hierarchical_data_and_the_closure_property/exercise_2.52.md
@@ -0,0 +1,115 @@
+# Exercise 2.52
+
+> Make changes to the square limit of `wave` shown in Figure 2.9 by working at each of the levels described above.
+> In particular:
+>
+> 1. Add some segments to the primitive `wave` painter of Exercise 2.49 (to add a smile, for example).
+>
+> 2. Change the pattern constructed by `corner-split` (for example, by using only one copy of the `up-split` and `right-split` images instead of two).
+>
+> 3. Modify the version of `square-limit` that uses `square-of-four` so as to assemble the corners in a different pattern.
+>    (For example, you might make the big Mr.~Rogers look outward from each corner of the square.)
+
+
+
+### 1.
+
+We reuse the auxiliary procedure `make-path` from Exercise 2.49:
+```scheme
+(define (make-path coords)
+  (define (coords-to-vects coords)
+    (if (null? coords)
+        '()
+        (cons (make-vect (car coords)
+                         (cadr coords))
+              (coords-to-vects (cddr coords)))))
+  (define (iter vects)
+    (if (null? (cdr vects))
+        '()
+        (cons (make-segment (car vects)
+                            (cadr vects))
+              (iter (cdr vects)))))
+  (iter (coords-to-vects coords)))
+```
+We modify `wave` by adding a face:
+```scheme
+(define new-wave
+  (let ((top-left (make-path (list 0.00 0.85
+                                   0.15 0.60
+                                   0.30 0.65
+                                   0.40 0.65
+                                   0.35 0.85
+                                   0.40 1.00)))
+        (left-eye (make-path (list 0.40 0.85
+                                   0.45 0.85)))
+        (mouth (make-path (list 0.50 0.78
+                                0.45 0.70
+                                0.55 0.70
+                                0.50 0.78)))
+        (right-eye (make-path (list 0.55 0.85
+                                    0.60 0.85)))
+        (top-right (make-path (list 0.60 1.00
+                                    0.65 0.85
+                                    0.60 0.65
+                                    0.75 0.65
+                                    1.00 0.35)))
+        (bottom-right (make-path (list 1.00 0.15
+                                       0.60 0.45
+                                       0.75 0.00)))
+        (bottom (make-path (list 0.60 0.00
+                                 0.50 0.30
+                                 0.40 0.00)))
+        (bottom-left (make-path (list 0.25 0.00
+                                      0.35 0.50
+                                      0.30 0.60
+                                      0.15 0.40
+                                      0.00 0.65))))
+    (segments->painter (append top-left
+                               left-eye
+                               mouth
+                               right-eye
+                               top-right
+                               bottom-right
+                               bottom
+                               bottom-left))))
+```
+The resulting image is as follows:
+
+![new wave procedure](new-wave.png "new-wave")
+
+
+
+### 2.
+
+We modify the procedure `corner-split` as follows:
+```scheme
+(define (new-corner-split painter n)
+  (if (= n 0)
+      (below (beside painter painter)
+             (beside painter painter))
+      (let ((up (up-split painter n))
+            (right (right-split painter n)))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (new-corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+```
+The resulting image looks as follows:
+
+![new corner-split procedure](new-corner-split.png "new-corner-split")
+
+
+
+### 3.
+
+Wo can arrange the courners in a rotated way:
+```scheme
+(define (new-square-limit painter n)
+  (let ((combine4 (square-of-four rotate90 identity
+                                  rotate180 rotate270)))
+    (combine4 (corner-split painter n))))
+```
+The resulting image looks as follows:
+
+![new square-limit procedure](new-square-limit.png "new-square-limit")
---
+++ 2_building_abstractions_with_data/2.2_hierarchical_data_and_the_closure_property/exercise_2.52.scm
@@ -0,0 +1,151 @@
+;;; The following code can be tested in DrRacket with the SICP Collections
+;;; (https://docs.racket-lang.org/sicp-manual/index.html) installed.
+
+#lang sicp
+(#%require sicp-pict)
+
+(define (make-path coords)
+  (define (coords-to-vects coords)
+    (if (null? coords)
+        '()
+        (cons (make-vect (car coords)
+                         (cadr coords))
+              (coords-to-vects (cddr coords)))))
+  (define (iter vects)
+    (if (null? (cdr vects))
+        '()
+        (cons (make-segment (car vects)
+                            (cadr vects))
+              (iter (cdr vects)))))
+  (iter (coords-to-vects coords)))
+
+(define wave
+  (let ((top-left (make-path (list 0.00 0.85
+                                   0.15 0.60
+                                   0.30 0.65
+                                   0.40 0.65
+                                   0.35 0.85
+                                   0.40 1.00)))
+        (top-right (make-path (list 0.60 1.00
+                                    0.65 0.85
+                                    0.60 0.65
+                                    0.75 0.65
+                                    1.00 0.35)))
+        (bottom-right (make-path (list 1.00 0.15
+                                       0.60 0.45
+                                       0.75 0.00)))
+        (bottom (make-path (list 0.60 0.00
+                                 0.50 0.30
+                                 0.40 0.00)))
+        (bottom-left (make-path (list 0.25 0.00
+                                      0.35 0.50
+                                      0.30 0.60
+                                      0.15 0.40
+                                      0.00 0.65))))
+    (segments->painter (append top-left
+                               top-right
+                               bottom-right
+                               bottom
+                               bottom-left))))
+
+(define (identity x) x)
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter
+               (beside smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-of-four tl tr bl br)
+  (lambda (painter)
+    (let ((top (beside (tl painter) (tr painter)))
+          (bottom (beside (bl painter) (br painter))))
+      (below bottom top))))
+
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four flip-horiz identity
+                                  rotate180 flip-vert)))
+    (combine4 (corner-split painter n))))
+
+
+
+;;; First modification
+
+(define new-wave
+  (let ((top-left (make-path (list 0.00 0.85
+                                   0.15 0.60
+                                   0.30 0.65
+                                   0.40 0.65
+                                   0.35 0.85
+                                   0.40 1.00)))
+        (left-eye (make-path (list 0.40 0.85
+                                   0.45 0.85)))
+        (mouth (make-path (list 0.50 0.78
+                                0.45 0.70
+                                0.55 0.70
+                                0.50 0.78)))
+        (right-eye (make-path (list 0.55 0.85
+                                    0.60 0.85)))
+        (top-right (make-path (list 0.60 1.00
+                                    0.65 0.85
+                                    0.60 0.65
+                                    0.75 0.65
+                                    1.00 0.35)))
+        (bottom-right (make-path (list 1.00 0.15
+                                       0.60 0.45
+                                       0.75 0.00)))
+        (bottom (make-path (list 0.60 0.00
+                                 0.50 0.30
+                                 0.40 0.00)))
+        (bottom-left (make-path (list 0.25 0.00
+                                      0.35 0.50
+                                      0.30 0.60
+                                      0.15 0.40
+                                      0.00 0.65))))
+    (segments->painter (append top-left
+                               left-eye
+                               mouth
+                               right-eye
+                               top-right
+                               bottom-right
+                               bottom
+                               bottom-left))))
+
+;;; Second modification
+
+(define (new-corner-split painter n)
+  (if (= n 0)
+      (below (beside painter painter)
+             (beside painter painter))
+      (let ((up (up-split painter n))
+            (right (right-split painter n)))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (new-corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+;;; Third modification
+
+(define (new-square-limit painter n)
+  (let ((combine4 (square-of-four rotate90 identity
+                                  rotate180 rotate270)))
+    (combine4 (corner-split painter n))))
